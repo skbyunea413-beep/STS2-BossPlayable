d022fad52c066dcf72eb4ce852de47703a510857
nagis <nagis@users.noreply.github.com>
2026-05-10 23:41:35 +0900

    Rework Peak of Folly as uncommon card generation

--- src/PrismModCode/Cards/Uncommon/PeakOfFolly.cs
+++ src/PrismModCode/Cards/Uncommon/PeakOfFolly.cs
@@ -8,11 +8,11 @@ public sealed class PeakOfFolly : PrismCard
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new CardsVar(7),
-        new DynamicVar("PlayedCards", 3m),
+        new CardsVar(4),
+        new DynamicVar("Rocks", 2m),
     ];
 
-    public PeakOfFolly() : base(3, CardType.Skill, CardRarity.Common, TargetType.Self) { }
+    public PeakOfFolly() : base(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self) { }
 
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
@@ -22,23 +22,26 @@ public sealed class PeakOfFolly : PrismCard
             return;
         }
 
-        List<CardModel> rocks = [];
         for (int i = 0; i < base.DynamicVars.Cards.IntValue; i++)
         {
-            var rock = base.CombatState.CreateCard<GiantRock>(base.Owner);
-            if (base.IsUpgraded)
-            {
-                CardCmd.Upgrade(rock);
-            }
+            await PrismRandomCardHelper.AddRandomCardToHand(ctx, base.Owner, IsOtherCharacterCommonCard);
+        }
 
+        List<CardModel> rocks = [];
+        for (int i = 0; i < base.DynamicVars["Rocks"].IntValue; i++)
+        {
+            var rock = base.CombatState.CreateCard<GiantRock>(base.Owner);
             rocks.Add(rock);
         }
 
         await CardPileCmd.AddGeneratedCardsToCombat(rocks, PileType.Discard, base.Owner);
+    }
 
-        for (int i = 0; i < base.DynamicVars["PlayedCards"].IntValue; i++)
-        {
-            await PrismRandomCardHelper.AutoPlayRandomCard(ctx, base.Owner, card => card.Rarity == CardRarity.Common);
-        }
+    protected override void OnUpgrade() => base.DynamicVars["Rocks"].UpgradeValueBy(-1m);
+
+    private static bool IsOtherCharacterCommonCard(CardModel card)
+    {
+        return card.Rarity == CardRarity.Common
+            && ModelDb.AllCharacterCardPools.Contains(card.Pool);
     }
 }
